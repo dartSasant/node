6acde72e202463f43203301293305816096dcbb8
dartSasant <sasantpudasaini@gmail.com>
2026-02-10 14:16:55 +0545

    project setup

---
+++ model/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+
+    },
+    lastName:{
+        type: String,
+        required: true,
+
+    },
+    email:{
+        type: String,
+        required: true,
+
+    },
+    password:{
+        type: String,
+        required: true,
+
+    },
+    isAdmin:{
+        type: String,
+        default: false
+    }
+    
+})
+
+module.exports = userModel;
