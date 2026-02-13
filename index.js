175b1cf3417720328671974e21a94b37e1a217ff
dartSasant <sasantpudasaini@gmail.com>
2026-02-13 14:17:31 +0545

    express completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello Wold")
+const express = require("express");
+
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req,res) => {
+    res.send("L is better than kira")
+});
